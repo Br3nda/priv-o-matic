ff2e0474469b885a6585387763b6fbb8021548c9
Awesome Code <team@xinminlabs.com>
2018-09-05 19:12:29 +0000

    Auto corrected by following Javascript import/order

--- tests/unit/initializers/load-fixtures-test.js
+++ tests/unit/initializers/load-fixtures-test.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
-import { initialize } from '../../../initializers/load-fixtures';
 import { module, test } from 'qunit';
+import { initialize } from '../../../initializers/load-fixtures';
 
 var container, application;
 
