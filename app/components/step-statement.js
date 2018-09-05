eb6048ff032a053c4425c9564069616cb47d9fd2
Awesome Code <team@xinminlabs.com>
2018-09-05 19:27:08 +0000

    Auto corrected by following Javascript space-before-blocks

--- app/components/step-statement.js
+++ app/components/step-statement.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   //Do we check the Check boxes?
-  keptSecurely: function (){
+  keptSecurely: function () {
     //have they filled in the secure method?
     return !! (this.model.get('statement.secureMethodOne'));
   }.property('model.statement.secureMethodOne'),
-  dataDestruction: function (){
+  dataDestruction: function () {
     //have they filled in the secure method?
     return !! (this.model.get('statement.dataRetentionTime'));
   }.property('model.statement.dataRetentionTime'),
